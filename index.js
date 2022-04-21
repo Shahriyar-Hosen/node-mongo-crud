9d4d52b7dd5a4bb085e3a706d0f1b067ce57efb2
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-21 10:38:18 +0600

    node-mongo-crud

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+// user: dbuser1
+// pass: cWMeLT0Yzucz89hH
+
+const uri =
+  "mongodb+srv://dbuser1:cWMeLT0Yzucz89hH@cluster0.d76bb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run(){
+  try{
+    await client.connect()
+    const userCollection = client.db("foodExpress").collection("user");
+    const user = {name: 'salman', email: 'salman@mail.com'};
+    const result = await userCollection.insertOne(user);
+    console.log(`User insert with id: ${result.insertedId}`);
+  }
+  finally{
+    await client.close()
+  }
+}
+
+run().catch(console.dir)
+
+app.get("/", (req, res) => {
+  res.send("Running My Node Mongo Server");
+});
+
+app.listen(port, () => {
+  console.log("CRUD Server is Running ");
+});
