e68def571f1bc2351c35aafa67104d6f77939f7c
shahriyahosen <me.shahriyarhosen@gmail.com>
2022-04-24 02:20:11 +0600

    dynamic user name & pass set

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ app.use(express.json());
 // user: dbuser1
 // pass: cWMeLT0Yzucz89hH
 
-const uri =
-  "mongodb+srv://dbuser1:cWMeLT0Yzucz89hH@cluster0.d76bb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d76bb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
